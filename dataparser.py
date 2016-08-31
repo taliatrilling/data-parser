441695ac47a1b19f52573c195ebe4bde48c5f133
Talia <taliatrilling@gmail.com>
2016-08-30 22:35:45 -0700

    changed to csv format

--- dataparser.py
+++ dataparser.py
@@ -1,36 +1,27 @@
-# import numpy as np
-# import openpyxl as op
-
-
 import pandas as pd
-from xml.sax import ContentHandler, parse
+import numpy as np
+import openpyxl as op
 
-# Reference https://goo.gl/KaOBG3
-class ExcelHandler(ContentHandler):
-    def __init__(self):
-        self.chars = [  ]
-        self.cells = [  ]
-        self.rows = [  ]
-        self.tables = [  ]
-    def characters(self, content):
-        self.chars.append(content)
-    def startElement(self, name, atts):
-        if name=="Cell":
-            self.chars = [  ]
-        elif name=="Row":
-            self.cells=[  ]
-        elif name=="Table":
-            self.rows = [  ]
-    def endElement(self, name):
-        if name=="Cell":
-            self.cells.append(''.join(self.chars))
-        elif name=="Row":
-            self.rows.append(self.cells)
-        elif name=="Table":
-            self.tables.append(self.rows)
+condition_sex_age = pd.read_csv("condition1.csv",
+    usecols = [13, 85, 172])
 
-excelHandler = ExcelHandler()
-parse('Aug21RJESdataacrossgroups.xml', excelHandler)
+df = pd.DataFrame(condition_sex_age, columns = ["CONDITION", "Sex:", "ACTUAL AGE"])
 
-df1 = pd.DataFrame(excelHandler.tables, excelHandler.tables)
-print df1
+bycondition1 = df.groupby(by = "CONDITION").get_group("1").apply(pd.to_numeric)
+
+bycondition2 = df.groupby(by = "CONDITION").get_group("2").apply(pd.to_numeric)
+
+bycondition3 = df.groupby(by = "CONDITION").get_group("3").apply(pd.to_numeric)
+
+bycondition4 = df.groupby(by = "CONDITION").get_group("4").apply(pd.to_numeric)
+
+bycondition5 = df.groupby(by = "CONDITION").get_group("5").apply(pd.to_numeric)
+
+bycondition6 = df.groupby(by = "CONDITION").get_group("6").apply(pd.to_numeric)
+
+print bycondition1.describe()
+print bycondition2.describe()
+print bycondition3.describe()
+print bycondition4.describe()
+print bycondition5.describe()
+print bycondition6.describe()
